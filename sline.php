fccb1a8369ab152194b69ae3df185b5bfc5eb9f9
maxzpr <maxzpr1@gmail.com>
2017-02-23 21:49:49 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -3,5 +3,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
-var_dump($bot);
+$content = file_get_contents('php://input');
+$events = json_decode($content, true);
+$ReplyToken = LINE\LINEBot\Event\MessageEvent($events)->getReplyToken();
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($ReplyToken, $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
