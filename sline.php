a5dc66b6f136b28312d8a79b2fcc5b6ea8bafcfe
maxzpr <maxzpr1@gmail.com>
2017-02-23 19:05:55 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
-require "LINEBot/HTTPClient/CurlHTTPClient";
-require "LINEBot";
+require "./LINEBot/HTTPClient/CurlHTTPClient.php";
+require "./LINEBot.php";
 $httpClient = new CurlHTTPClient($access_token);
 $bot = new LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
 $textMessageBuilder = $bot->TextMessageBuilder('hello test');
