351ad8c4e2a62d72a3c3b94b00f04a33bca0c98c
maxzpr <maxzpr1@gmail.com>
2017-02-23 19:59:47 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . './vendor/autoload.php';
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
