348e2b3352280dc0174bf426b5a48c73ea0d8408
maxzpr <maxzpr1@gmail.com>
2017-02-23 18:40:14 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
+$httpClient = new \LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
 $response = $bot->replyText('<reply token>', 'hello!');
 ?>
