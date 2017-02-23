48a8df17ec62634554369710524878eb62c1b45d
maxzpr <maxzpr1@gmail.com>
2017-02-23 17:38:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,6 @@ require 'Weather.php';
 $w = new Weather;
 $data = $w->getWeather("Chiangmai,th");
 $weather = $data["item"]["forecast"];
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-$response = $bot->getProfile('<userId>');
-if ($response->isSucceeded()) {
-    $profile = $response->getJSONDecodedBody();
-    echo $profile['displayName'];
-    echo $profile['pictureUrl'];
-    echo $profile['statusMessage'];
-}
 ?>
 <html>
     <head>
