f24fb762b21b12473b7c870809c260068f40e229
maxzpr <maxzpr1@gmail.com>
2017-02-23 19:32:55 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
-$httpClient = new LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
+$httpClient = new __DIR__.\LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new __DIR__ .\LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
 $textMessageBuilder = new LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('<to>', $textMessageBuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
