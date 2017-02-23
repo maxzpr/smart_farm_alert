29e35f561b48440d0fc17be755cd4de6bc39b0fc
maxzpr <maxzpr1@gmail.com>
2017-02-23 20:56:10 +0700

    Update sline.php

--- sline.php
+++ sline.php
@@ -3,6 +3,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
-$myfile = fopen("user.json", "w");
-file_put_contents("user.json",$myfile."save");
+var_dump($bot);
 ?>
