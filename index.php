18e65c6feadbafa1baa91834cd12d1e247d43796
maxzpr <maxzpr1@gmail.com>
2017-02-23 18:06:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__FILE__) . '/Weather.php';
+include(dirname(__FILE__) . '/Weather.php');
 $w = new Weather;
 $data = $w->getWeather("Chiangmai,th");
 $weather = $data["item"]["forecast"];
