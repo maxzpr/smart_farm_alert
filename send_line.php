6a4262e8c04de23d4bf609261526977188fb2ae9
maxzpr <maxzpr1@gmail.com>
2017-02-26 17:47:14 +0700

    Update send_line.php

--- send_line.php
+++ send_line.php
@@ -3,12 +3,12 @@
   $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
+  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
   $file = "user.json";
   $fr = file_get_contents($file);
   $arr = json_decode($fr, TRUE);
-  $response = $bot->pushMessage($arr,{
-    "type": "text",
-    "text": "Hello, world"
-  });
+  foreach($arr as $a){
+     $response = $bot->pushMessage($a,$textMessageBuilder);
+  }
   echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
