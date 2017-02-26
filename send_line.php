853bd817d400cf7896cfd83061099ed0e8cd1c82
maxzpr <maxzpr1@gmail.com>
2017-02-26 18:41:18 +0700

    Update send_line.php

--- send_line.php
+++ send_line.php
@@ -2,14 +2,6 @@
     if(!empty($_POST["msg"])){
     require_once __DIR__ . '/vendor/autoload.php';
     $msg = $_POST["msg"];
-    
-    $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($_POST["msg"]);
-    $file = "user.json";
-    $fr = file_get_contents($file);
-    $arr = json_decode($fr, TRUE);
     if($msg=='w'){
         require "./weather.php";
         $w = new Weather;
@@ -17,9 +9,15 @@
         $weather = $data["item"]["forecast"];
         $msg = $weather[0]["date"]."Weather today is ".$weather[0]["text"];
     }
-    $m = $msg;
+    $access_token = 'VPIFT2PSZgYu3m787JdL+UrikOJLempDam0gLNe2yBt5YmsA8Sz0tFjfr+jZx6Xivxk4NDqXmVFPhIARIjA43RES6H/Twv2AkRCzJ0BwlQMUNUJfmPHeFITKEHPGHXqE/J9ea2FfEXMmchv00nwfKAdB04t89/1O/w1cDnyilFU=';
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75584d370097584887a2b1e843de141c']);
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($msg);
+    $file = "user.json";
+    $fr = file_get_contents($file);
+    $arr = json_decode($fr, TRUE);
      foreach($arr as $a){
-           $response = $bot->pushMessage($a,$m);
+           $response = $bot->pushMessage($a,$textMessageBuilder);
         }
         if($response->getHTTPStatus() == '200'){
           echo '1';
