ca9cb94279e129b69a76571ea645d98ad3e9b2ca
maxzpr <maxzpr1@gmail.com>
2017-03-03 19:54:04 +0700

    Update send_line.php

--- send_line.php
+++ send_line.php
@@ -8,8 +8,9 @@
         $data = $w->getWeather("Chiangmai,th");
         $weather = $data["item"]["forecast"];
         //$msg = $weather[0]["date"]."Weather today is ".$weather[0]["text"];
-        foreach($weather as $key=>$value){
-            $msg.=$key."".$value["date"]."Weather today is ".$value["text"];
+        $i = 1;
+        foreach($weather as $value){
+            $msg.="\n".$i++.$key.$value["date"]."Weather today is ".$value["text"];
         }
         //$msg = print_r($weather,true);
     }
